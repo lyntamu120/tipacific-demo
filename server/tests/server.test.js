ce24767ffd2068b9333b17f9d1f89b8f4aadfdb1
Xining Wang <icemeTa@main-10-230-34-233.tamulink.tamu.edu>
2018-07-03 14:53:39 -0500

    Add test: GET /tips/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,16 +1,20 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('../server');
 const { Tip } = require('../models/tip');
 
 const tips = [{
+  _id: new ObjectID,
   text: "First test tip",
   tag: 'Guitar'
 }, {
+  _id: new ObjectID,
   text: "Second test tip",
   tag: 'Dota2'
 }];
+
 beforeEach((done) => {
   Tip.remove({}).then(() => {
     return Tip.insertMany(tips);
@@ -19,23 +23,25 @@ beforeEach((done) => {
 
 describe('POST /tip', () => {
   it('should create a new tip', (done) => {
-    var text = 'Test post tip';
-    var tag = 'PE';
+    let tip = {
+      text: 'Test post tip',
+      tag: 'PE'
+    }
     request(app)
       .post('/tips')
-      .send({text, tag})
+      .send(tip)
       .expect(200)
       .expect((res) => {
-        expect(res.body.text).toBe(text);
+        expect(res.body.tip.text).toBe(tip.text);
       })
       .end((err, res) => {
         if (err) {
           return done(err);
         }
 
-        Tip.find({text}).then((tips) => {
+        Tip.find({text: tip.text}).then((tips) => {
           expect(tips.length).toBe(1);
-          expect(tips[0].text).toBe(text);
+          expect(tips[0].text).toBe(tip.text);
           done();
         }).catch((e) => done(e));
       });
@@ -54,7 +60,7 @@ describe('POST /tip', () => {
         Tip.find().then((tips) => {
           expect(tips.length).toBe(2);
           done();
-        }).catch((e) => done());
+        }).catch((e) => done(e));
       });
   });
 });
@@ -67,6 +73,34 @@ describe('GET /tips', () => {
       .expect((res) => {
         expect(res.body.tips.length).toBe(2);
       })
+      .end(done);
+  });
+});
+
+describe('GET /tips/:id', () => {
+  it('should get the first tip', (done) => {
+    const id = tips[0]._id.toHexString();
+    request(app)
+      .get(`/tips/${id}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.tip.text).toBe(tips[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return a 404 if the id is not valid', (done) => {
+    request(app)
+      .get(`/tips/1`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return a 404 if the id is not in the database', (done) => {
+    let id = new ObjectID;
+    request(app)
+      .get(`/tips/${id}`)
+      .expect(404)
       .end(done);
   });
 });
